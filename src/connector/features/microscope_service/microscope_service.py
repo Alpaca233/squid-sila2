a2d3faf47907e926aee218130beafa5371867bd6
You Yan <yy2900@columbia.edu>
2025-03-02 20:45:40 -0800

    updated laser autofocus

--- src/connector/features/microscope_service/microscope_service.py
+++ src/connector/features/microscope_service/microscope_service.py
@@ -18,6 +18,16 @@ class MicroscopeService(MicroscopeServiceBase):
         self.microscope = microscope
         self.coordinates = None
         self.names = None
+        self.channel_names = {
+            '0': 'BF LED matrix full',
+            '1': 'DF LED matrix',
+            '2': 'BF LED matrix low NA',
+            '405': 'Fluorescence 405 nm Ex',
+            '488': 'Fluorescence 488 nm Ex',
+            '561': 'Fluorescence 561 nm Ex',
+            '638': 'Fluorescence 638 nm Ex',
+            '730': 'Fluorescence 730 nm Ex',
+        }
 
     async def get_stage_position(self) -> str:
         pos = ','.join([str(self.microscope.get_x()), str(self.microscope.get_y()), str(self.microscope.get_z())])
@@ -28,13 +38,22 @@ class MicroscopeService(MicroscopeServiceBase):
         return True
 
     async def move_to(self, x_mm: float, y_mm: float, z_um: float) -> bool:
-        self.microscope.move_to_position(x, y, z_um / 1000)
+        self.microscope.move_to_position(x_mm, y_mm, z_um / 1000)
+        return True
+    
+    async def load_profile(self, profile_name: str) -> bool:
+        self.microscope.configurationManager.load_profile(profile_name)
         return True
 
-    async def select_wells(self, wellplate_format: str, selection: str, scan_size_mm: float = None, overlap: float = 10) -> bool:
-        self.coordinates, self.names = self.microscope.get_scan_coordinates_from_selected_wells(wellplate_format, selection, scan_size_mm, overlap)
+    async def set_objective(self, objective_name: str) -> bool:
+        self.microscope.set_objective(objective_name)
         return True
 
-    async def perform_scanning(self, path: str, z_pos_um: float, channels: str = '0') -> bool:
-        self.microscope.perform_scanning(path, z_pos_um, channels.split(','), self.coordinates, self.names)
+    async def select_wells_to_scan(self, wellplate_format: str, selection: str, scan_size_mm: float = None, overlap: float = 10) -> bool:
+        self.coordinates, self.names = self.microscope.set_coordinates(wellplate_format, selection, scan_size_mm, overlap)
+        return True
+
+    async def perform_scanning(self, path: str, experiment_ID: str, z_pos_um: float, channels: str = '0', use_laser_af: bool = True) -> bool:
+        channel_list = [self.channel_names[ch.strip()] for ch in channels.split(',')]
+        self.microscope.perform_scanning(path, experiment_ID, z_pos_um, channel_list, use_laser_af)
         return True
--- src/connector/features/microscope_service/microscope_service_base.py
+++ src/connector/features/microscope_service/microscope_service_base.py
@@ -55,7 +55,27 @@ class MicroscopeServiceBase(sila.Feature, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     @sila.UnobservableCommand()
     @sila.Response(name="Result")
-    async def select_wells(self, wellplate_format: str, selection: str, scan_size_mm: float = None, overlap: float = 10) -> bool:
+    async def load_profile(self, profile_name: str) -> bool:
+        """
+        Load a profile.
+        .. parameter:: profile_name: name of the profile to load
+        .. return:: execution result. True: successful; False: failed
+        """
+    
+    @abc.abstractmethod 
+    @sila.UnobservableCommand()
+    @sila.Response(name="Result")
+    async def set_objective(self, objective_name: str) -> bool:
+        """
+        Set the objective.
+        .. parameter:: objective_name: name of the objective to set
+        .. return:: execution result. True: successful; False: failed
+        """
+
+    @abc.abstractmethod
+    @sila.UnobservableCommand()
+    @sila.Response(name="Result")
+    async def select_wells_to_scan(self, wellplate_format: str, selection: str, scan_size_mm: float = None, overlap: float = 10) -> bool:
         """
         Select wells and generate fov coordinates to scan
         .. parameter:: wellplate_format: a str such as '384 well plate', '96 well plate' or the name of your custom wellplate
@@ -68,11 +88,13 @@ class MicroscopeServiceBase(sila.Feature, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     @sila.UnobservableCommand()
     @sila.Response(name="Result")
-    async def perform_scanning(self, path: str, z_pos_um: float, channels: str = '0') -> bool:
+    async def perform_scanning(self, path: str, experiment_ID: str, z_pos_um: float, channels: str = '0', use_laser_af: bool = True) -> bool:
         """
         Start scanning at with xy coordinates generated previously and a given z position. Save acquired images.
         .. parameter:: path: path to save images
+        .. parameter:: experiment_ID: experiment ID
         .. parameter:: z_pos_um: z position in um.
-        .. parameter:: channels: select channels to image. 0: BF, 1: DF, 2: 405 nm, 3: 488 nm, 4: 561 nm, 5: 638 nm, 6: 730 nm. Use a str such as '0,2,4'
+        .. parameter:: channels: select channels to image. 0: BF, 1: DF, 2: BF low NA, 405: 405 nm, 488: 488 nm, 561: 561 nm, 638: 638 nm, 730: 730 nm. Use a str such as '0,2,405'
+        .. parameter:: use_laser_af: use laser autofocus
         .. return:: execution result. True: successful; False: failed
         """
